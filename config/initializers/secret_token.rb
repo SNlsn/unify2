6ba4ea5f9bf7c1d8b7769770f1496806fefe5b2c
Steve Nelson <stv.nlsn@gmail.com>
2012-02-20 12:09:48 -0500

    Initial commit of project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unify2::Application.config.secret_token = 'b5ca296e45cf5e3205276825f13a657d7ce3c43a7bbc0a8958e09a17c2f9ab5ab14a3767d4a0b23948816755dd45cfd8093b1ed3c619520e98506f7137928b47'
